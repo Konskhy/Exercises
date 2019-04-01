f6806f044f7d22c458d13b44e937eaf5190609bf
Konskhy <konski88@gmail.com>
2019-04-01 23:47:20 +0100

    implement collections methods

--- src/com/company/konski/Main.java
+++ src/com/company/konski/Main.java
@@ -1,5 +1,9 @@
 package com.company.konski;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -7,6 +11,18 @@ public class Main {
         odeon.listSeats();
         odeon.reserveSeat("B12");
         odeon.reserveSeat("B12");
+        List<Theatre.Seat> listCopy = new ArrayList<>(odeon.seats);
+        Collections.reverse(listCopy);
+        printList(listCopy);
+        Collections.sort(listCopy,Theatre.PRICE_COMP);
+        printList(listCopy);
 
+    }
+
+    public static void printList(List <Theatre.Seat>listCopy){
+        for (Theatre.Seat seat: listCopy){
+            System.out.print("#"+seat.getSeatNumber()+" $"+seat.getSeatPrice()+" | ");
+
+        }System.out.println();
     }
 }
--- src/com/company/konski/Theatre.java
+++ src/com/company/konski/Theatre.java
@@ -1,17 +1,30 @@
 package com.company.konski;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat>seats = new ArrayList<>();
-
+    public List<Seat>seats = new ArrayList<>();
+    static final Comparator<Seat> PRICE_COMP = new Comparator<Seat>() {
+        @Override
+        public int compare(Seat o1, Seat o2) {
+            if (o1.getSeatPrice()>o2.getSeatPrice()) return 1;
+            else if (o1.getSeatPrice()<o2.getSeatPrice()) return -1;
+            else {
+                 return o1.compareTo(o2);
+            }
+        }
+    };
+    private double price;
     public Theatre(String theatreName, int rows, int seatsPerRow){
         this.theatreName = theatreName;
         int lastRow = 'A'+rows-1;
         for (char i='A'; i<=lastRow;i++){
             for (int j=1; j<=seatsPerRow;j++){
+                if (i>='A'&&i<='C'){
+                    if (j>=3&&j<=seatsPerRow-3) price=15.00;
+                    else price=7.00;
+                } else price=10.00;
                 Seat seat = new Seat(i+String.format("%02d",j));
                 seats.add(seat);
             }
@@ -20,10 +33,11 @@ public class Theatre {
     public List<Seat> getSeats(){
         return seats;
     }
-
     public String getTheatreName() {
         return theatreName;
     }
+    //todo binary search
+
     public boolean reserveSeat(String requestedSeat) {
         Seat reqSeat = null;
         for (Seat seat : seats) {
@@ -37,17 +51,24 @@ public class Theatre {
         return true;}
     }
     public void listSeats(){
-        for (Seat seat:seats) System.out.print("#"+seat.getSeatNumber()+" ");
+        for (Seat seat:seats) System.out.print("#"+seat.getSeatNumber()+" $"+seat.getSeatPrice()+" | ");
         System.out.println();
     }
-//todo implement comparable interface
 
-    private class Seat{
+
+
+    public class Seat implements Comparable <Seat>{
         private final String seatNumber;
         private boolean reserved;
+        private double seatPrice;
         private Seat(String seatNumber){
             this.seatNumber=seatNumber;
             this.reserved=false;
+            this.seatPrice=price;
+        }
+
+        public double getSeatPrice() {
+            return seatPrice;
         }
 
         public String getSeatNumber() {
@@ -58,8 +79,13 @@ public class Theatre {
                 System.out.println("Seat already reserved");
                 return true;
             } else reserved=true;
-            System.out.println("Seat "+getSeatNumber()+" has been reserved!");
+            System.out.println("Seat "+getSeatNumber()+" has been reserved, please pay "+getSeatPrice()+"$");
             return false;
+        }
+
+        @Override
+        public int compareTo(Seat o) {
+            return getSeatNumber().compareToIgnoreCase(o.getSeatNumber());
         }
     }
 }
