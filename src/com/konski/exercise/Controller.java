23f3ff177f101c2daf507c44f2d6ed4dec77615a
konski <konski88@gmail.com>
2019-04-30 12:18:10 +0100

    save/load to file added

--- src/com/konski/exercise/Controller.java
+++ src/com/konski/exercise/Controller.java
@@ -1,6 +1,9 @@
 package com.konski.exercise;
 
+import com.konski.exercise.datamodel.TodoData;
 import com.konski.exercise.datamodel.TodoItem;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -9,6 +12,7 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextArea;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,26 +26,41 @@ public class Controller {
     private Label deadline;
 
     public void initialize(){
-        TodoItem item1 = new TodoItem("Resupply weed", "Ring neighbor to arrange some weed, we're running low",
-                LocalDate.of(2019,4,30));
-        TodoItem item2 = new TodoItem("Java Project", "Finish the project, cleanup, annotations, documentation",
-                LocalDate.of(2019,5,5));
-        TodoItem item3 = new TodoItem("Laundry", "Do the bleedin laundry, don't forget the fencing gear",
-                LocalDate.of(2019,5,1));
-        TodoItem item4 = new TodoItem("Ring mom", "Check what's the story with first communion arrangements",
-                LocalDate.of(2019,5,15));
-        TodoItem item5 = new TodoItem("Communion", "Travel to Poland for godsdaughter first communion",
-                LocalDate.of(2019,5,25));
+//        TodoItem item1 = new TodoItem("Resupply weed", "Ring neighbor to arrange some weed, we're running low",
+//                LocalDate.of(2019,4,30));
+//        TodoItem item2 = new TodoItem("Java Project", "Finish the project, cleanup, annotations, documentation",
+//                LocalDate.of(2019,5,5));
+//        TodoItem item3 = new TodoItem("Laundry", "Do the bleedin laundry, don't forget the fencing gear",
+//                LocalDate.of(2019,5,1));
+//        TodoItem item4 = new TodoItem("Ring mom", "Check what's the story with first communion arrangements",
+//                LocalDate.of(2019,5,15));
+//        TodoItem item5 = new TodoItem("Communion", "Travel to Poland for godsdaughter first communion",
+//                LocalDate.of(2019,5,25));
+//
+//        todoItems = new ArrayList<>();
+//        todoItems.add(item1);
+//        todoItems.add(item2);
+//        todoItems.add(item3);
+//        todoItems.add(item4);
+//        todoItems.add(item5);
+//
+//        TodoData.getInstance().setTodoItems(todoItems);
 
-        todoItems = new ArrayList<>();
-        todoItems.add(item1);
-        todoItems.add(item2);
-        todoItems.add(item3);
-        todoItems.add(item4);
-        todoItems.add(item5);
+        todoListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<TodoItem>() {
+            @Override
+            public void changed(ObservableValue<? extends TodoItem> observableValue, TodoItem todoItem, TodoItem newItem) {
+                if (newItem!=null){
+                    TodoItem item = todoListView.getSelectionModel().getSelectedItem();
+                    detailsTextArea.setText(item.getDetails());
+                    DateTimeFormatter df = DateTimeFormatter.ofPattern("MMMM d, yyyy");
+                    deadline.setText(item.getDeadline().format(df));
+                }
+            }
+        });
 
-        todoListView.getItems().setAll(todoItems);
+        todoListView.getItems().setAll(TodoData.getInstance().getTodoItems());
         todoListView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        todoListView.getSelectionModel().selectFirst();
     }
     @FXML
     public void handleListView(){
--- src/com/konski/exercise/Main.java
+++ src/com/konski/exercise/Main.java
@@ -1,12 +1,24 @@
 package com.konski.exercise;
 
+import com.konski.exercise.datamodel.TodoData;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
+
+    @Override
+    public void init() throws Exception {
+        try{
+            TodoData.getInstance().loadTodoItems();
+        }catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -19,5 +31,14 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        try{
+            TodoData.getInstance().saveTodoItems();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
